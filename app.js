b3f41f431685302c8131c0337dda9534f027d195
DIMITRIOS KALAITZIS <d.kalaitzis@yahoo.com>
2020-12-09 17:43:37 +0000

    dev:3
    errors work
    not fully working db conn yet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
 
 var db = require("./models");
 const PORT = process.env.PORT || 3000;
@@ -23,6 +24,9 @@ app.listen(PORT, console.log(`Server started on ${PORT}`));
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 // Express Session
 app.use(session({
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("User", {
-        username: DataTypes.STRING,
+        name: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
         password2: DataTypes.STRING
