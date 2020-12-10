892d4ee59af12a1c3aac8d438a8ae6a0203ac34e
DIMITRIOS KALAITZIS <d.kalaitzis@yahoo.com>
2020-12-10 17:34:29 +0000

    Dev:5

--- routes/posts.js
+++ routes/posts.js
@@ -1,41 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const Post = require ('../models/Post');
 const { ensureAuthenticated } = require('../config/auth');
 
-// All Posts Page
-// router.get('/dashboard/posts', ensureAuthenticated, (req,res) => 
-//     res.render('posts', {
-//         name: req.user.name
-//     })); 
-
-// router.get('/dashboard/posts', (req,res) => res.render('login'));
-
-// Get posts
-router.get('/', (req, res) => 
-    Post.findAll()
-        .then(posts => {
-            console.log(posts);
-            res.sendStatus(200);
+// Display All Posts
+router.get('/posts',  (req,res) => 
+    res.render('posts', {
+        // name: req.user.name
+    }));  
+    db.Post.findAll()
+        .then(post => {
+            res.render('posts', {
+                content,
+                UserId
+            });
+            // console.log(posts);
+            // res.sendStatus(200);
         })
-        .cath(err => console.log(err)));
+        .cath(err => console.log(err));
 
-// Add Post
-router.get('/add', (req, res) => {
-    const data = {
-        title: 'Title'
-    }
-
-    let { content, user_id } = data;
-
+// Create Post
+router.post('/posts', (req, res) => {
     // Insert into table
-    Post.create({
+    const newPost = new db.Post({
         content,
-        user_id
-    })
-        .then(post => res.redirect('/posts'))
-        .catch(err => console.log(err));
+        UserId
+    });
+    db.User.create({content: newPost.content, UserId: newPost.UserId})
+    .then(user => {
+        req.flash('success_msg', 'Great Successful Post!!');
+        res.redirect('/home/posts');
+    }).catch(err => console.log(err))
 });
 
 
